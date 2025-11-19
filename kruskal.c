0ca31a6123fde85de1b942887bd3cc152f54b55f
Bibithyabinu <bibithyabinu23@gmail.com>
2025-11-19 13:49:31 +0530

    Add files via upload

---
+++ kruskal.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 30
+
+struct Edge {
+    int src, dest, weight;
+};
+
+struct Subset {
+    int parent, rank;
+};
+
+// Function to find set of an element i (with path compression)
+int find(struct Subset subsets[], int i) {
+    if (subsets[i].parent != i)
+        subsets[i].parent = find(subsets, subsets[i].parent);
+    return subsets[i].parent;
+}
+
+// Function to do union of two sets x and y (by rank)
+void Union(struct Subset subsets[], int x, int y) {
+    int xroot = find(subsets, x);
+    int yroot = find(subsets, y);
+
+    if (subsets[xroot].rank < subsets[yroot].rank)
+        subsets[xroot].parent = yroot;
+    else if (subsets[xroot].rank > subsets[yroot].rank)
+        subsets[yroot].parent = xroot;
+    else {
+        subsets[yroot].parent = xroot;
+        subsets[xroot].rank++;
+    }
+}
+
+// Comparison function for qsort
+int compareEdges(const void* a, const void* b) {
+    return ((struct Edge*)a)->weight - ((struct Edge*)b)->weight;
+}
+
+// Kruskal's algorithm
+void Kruskals(struct Edge edges[], int V, int E) {
+    struct Edge result[MAX]; 
+    int e = 0;  
+    int i = 0;  
+
+    qsort(edges, E, sizeof(edges[0]), compareEdges);
+
+    struct Subset* subsets = (struct Subset*)malloc(V * sizeof(struct Subset));
+
+    for (int v = 0; v < V; v++) {
+        subsets[v].parent = v;
+        subsets[v].rank = 0;
+    }
+
+    while (e < V - 1 && i < E) {
+        struct Edge next_edge = edges[i++];
+
+        int x = find(subsets, next_edge.src);
+        int y = find(subsets, next_edge.dest);
+
+        if (x != y) {
+            result[e++] = next_edge;
+            Union(subsets, x, y);
+        }
+    }
+
+    printf("\nEdges in the Minimum Spanning Tree:\n");
+    int minCost = 0;
+    for (i = 0; i < e; i++) {
+        printf("%d -- %d == %d\n", result[i].src, result[i].dest, result[i].weight);
+        minCost += result[i].weight;
+    }
+    printf("Minimum Cost of MST: %d\n", minCost);
+}
+
+int main() {
+    int V, E;
+    printf("Enter number of vertices: ");
+    scanf("%d", &V);
+
+    printf("Enter number of edges: ");
+    scanf("%d", &E);
+
+    struct Edge edges[MAX];
+
+    printf("Enter edges (src dest weight):\n");
+    for (int i = 0; i < E; i++) {
+        printf("Edge %d: ", i + 1);
+        scanf("%d %d %d", &edges[i].src, &edges[i].dest, &edges[i].weight);
+    }
+
+    Kruskals(edges, V, E);
+
+    return 0;
+}
